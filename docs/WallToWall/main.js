45ebbe03c683c017e7ef9416ee1288aa6e31745a
MarkViews <46404728+markviews@users.noreply.github.com>
2023-11-07 19:56:24 -0800

    added multiple ball support, fixed powerups

--- docs/WallToWall/main.js
+++ docs/WallToWall/main.js
@@ -1,159 +1,183 @@
 title = "Wall to Wall";
-
-description = `
-[Tap] Change wall color
-`;
-
-characters = [];
-//Wall variables
-let wallColors;
-let wallNumber;
-let lastWallNumber;
-let powerUp;
-let parallelUp;
-//Ball variables
-let ball;
-let ballSpeed;
-let ballDirection;
-
+description = `[Tap] Next wall`;
 options = {
   viewSize: { x: 100, y: 100 },
   theme: "shapeDark",
 };
 
+//Wall variables
+let powerUp;
+let parallelUp;
+let balls = [];
+let wallNumbers = [];
+
 function update() {
   if (!ticks) {
-    wallColors = ["white", "white", "white", "white"];
-    wallNumber = 0;
-    lastWallNumber = -1;
     powerUp = false;
     parallelUp = false;
-    // Initialize the ball
-    ball = { x: 50, y: 50, radius: 2 };
-    ballSpeed = 0.7;
-    ballDirection = { x: 0.5, y: 1 }; // Initial direction vector of the ball
+    balls = [];
+    wallNumbers = [0];
+    spawnBall();
   }
   OnClick();
   paintWalls();
   moveBall();
   handleBallCollision();
   paintBall();  
+}
 
+function spawnBall() {
+  balls.push({ 
+    x: 50,
+    y: 50,
+    radius: 2,
+    speed: 0.7,
+    direction: { x: Math.random(), y: Math.random() },
+    lastWallNumber: -1
+  });
 }
 
 function OnClick(){
+
   //Change wall color
   if (input.isJustPressed) {
-    wallColors[wallNumber] = "white";
-    wallNumber++;
-    if(wallNumber > 3){
-      wallNumber = 0;
-    }
-    wallColors[wallNumber] = "green";
+
+    let newNums = [];
+    wallNumbers.forEach((wallNumber) => {
+      wallNumber++;
+      if(wallNumber > 3) wallNumber = 0;
+      newNums.push(wallNumber);
+    });
+
+    wallNumbers = newNums;
   }
+
 }
 
 function paintWalls(){
-  //No Power up.
-  if(wallNumber == 0 && powerUp == false && parallelUp == false){
-    color(wallColors[0]);
-    rect(0, 0, 7, 150);
-  }
-  else if (wallNumber == 1 && powerUp == false && parallelUp == false){
-    color(wallColors[1]);
-    rect(0, 0, 100, 7);
-  }
-  else if (wallNumber == 2 && powerUp == false && parallelUp == false){
-    color(wallColors[2]);
-    rect(93, 0, 7, 150);
-  }
-  else if (wallNumber == 3 && powerUp == false && parallelUp == false){
-    color(wallColors[3]);
-    rect(0, 93, 100, 7);
-  }
+  wallNumbers.forEach((wallNumber) => {
 
-  //Power up (Right Angle)
-  if(wallNumber == 0 && powerUp == true && parallelUp == false){
-    color(wallColors[0]);
-    rect(0, 0, 7, 150);
-    rect(0, 0, 100, 7);
-  }
-  else if (wallNumber == 1 && powerUp == true && parallelUp == false){
-    color(wallColors[1]);
-    rect(0, 0, 100, 7);
-    rect(93, 0, 7, 150);
-  }
-  else if (wallNumber == 2 && powerUp == true && parallelUp == false){
-    color(wallColors[2]);
-    rect(93, 0, 7, 150);
-    rect(0, 93, 100, 7);
-  }
-  else if (wallNumber == 3 && powerUp == true && parallelUp == false){
-    color(wallColors[3]);
-    rect(0, 93, 100, 7);
-    rect(0, 0, 7, 150);
-  }
+    if(wallNumber == 0){
+      color("green");
+      rect(0, 0, 7, 150);
+    }
+    else if (wallNumber == 1){
+      color("green");
+      rect(0, 0, 100, 7);
+    }
+    else if (wallNumber == 2){
+      color("green");
+      rect(93, 0, 7, 150);
+    }
+    else if (wallNumber == 3){
+      color("green");
+      rect(0, 93, 100, 7);
+    }
 
-  //Parallel Power up (Parallel)
-  if((wallNumber == 0 || wallNumber == 2) && parallelUp == true){
-    color("green");
-    rect(0, 0, 7, 150);
-    rect(93, 0, 7, 150);
-  }
-  else if((wallNumber == 1 || wallNumber == 3) && parallelUp == true){
-    color("green");
-    rect(0, 0, 100, 7);
-    rect(0, 93, 100, 7);
-  }
+  });
 
 }
 
 //Creates the ball with specifications
 function paintBall(){
   color("black");
-  //Draw the ball
-  arc(ball.x, ball.y, ball.radius * 0.1);
+
+  //Draw the balls
+  balls.forEach((ball) => {
+    arc(ball.x, ball.y, ball.radius * 0.1);
+  });
 }
+
 //moves the ball around based on its speed and direction
-function moveBall(){
-  ball.x += ballSpeed * ballDirection.x;
-  ball.y += ballSpeed * ballDirection.y;
+function moveBall() {
+  balls.forEach((ball) => {
+    let magnitude = Math.sqrt(ball.direction.x**2 + ball.direction.y**2);
+    if (magnitude == 0) return;
+
+    let normalized_x = ball.direction.x / magnitude;
+    let normalized_y = ball.direction.y / magnitude;
+    ball.x += ball.speed * normalized_x;
+    ball.y += ball.speed * normalized_y;
+  });
 }
 //Checks if a green wall was hit
 function handleBallCollision() {
-  // Check collision with the screen boundaries (Lose p much)
-  if (ball.x - ball.radius < 0 || ball.x + ball.radius > 100) {
-    end();
-  }
-  if (ball.y - ball.radius < 0 || ball.y + ball.radius > 100) {
-    end();
-  }
 
-  // Check collision with green walls
-  if (wallColors[wallNumber] === "green") {
-    if (
-      (wallNumber === 0 && ball.x - ball.radius <= 7) ||
-      (wallNumber === 2 && ball.x + ball.radius >= 93)
-    ) {
-      if (lastWallNumber != wallNumber) {
-        lastWallNumber = wallNumber;
-        increaseScore();
-        ballDirection.x *= -1; // Reverse the x-direction
+  balls.forEach((ball) => {
+
+    // Check collision with the screen boundaries (Lose p much)
+    if (ball.x - ball.radius < 0 || ball.x + ball.radius > 100 || ball.y - ball.radius < 0 || ball.y + ball.radius > 100) {
+      const index = balls.indexOf(ball);
+      if (index > -1) {
+        balls.splice(index, 1);
       }
-    }
-    if (
-      (wallNumber === 1 && ball.y - ball.radius <= 7) ||
-      (wallNumber === 3 && ball.y + ball.radius >= 93)
-    ) {
-      if (lastWallNumber != wallNumber) {
-        lastWallNumber = wallNumber;
-        increaseScore();
-        ballDirection.y *= -1; // Reverse the y-direction
+
+      if (balls.length == 0) {
+        end();
       }
+
     }
-  }
+
+    // Check collision with green walls
+    wallNumbers.forEach((wallNumber) => {
+      if ((wallNumber === 0 && ball.x - ball.radius <= 7) || (wallNumber === 2 && ball.x + ball.radius >= 93)) {
+        if (ball.lastWallNumber != wallNumber) {
+          ball.lastWallNumber = wallNumber;
+          increaseScore();
+          ball.direction.x *= -1; // Reverse the x-direction
+        }
+      }
+
+      if ((wallNumber === 1 && ball.y - ball.radius <= 7) || (wallNumber === 3 && ball.y + ball.radius >= 93)) {
+        if (ball.lastWallNumber != wallNumber) {
+          ball.lastWallNumber = wallNumber;
+          increaseScore();
+          ball.direction.y *= -1; // Reverse the y-direction
+        }
+      }
+    });
+    
+  });
 }
 
 function increaseScore(){
   score++;
+
+  // spawn new ball every 2 points
+  if(score % 2 == 0) {
+    spawnBall();
+  }
+
+  // reset powerups
+  if (powerUp || parallelUp) {
+    parallelUp = false;
+    powerUp = false;
+    wallNumbers.pop();
+  }
+
+  // power up every 5 points
+  if(score % 5 == 0){
+    
+    // choose random power up
+    if(Math.random() < 0.5){
+      powerUp = true;
+    } else {
+      parallelUp = true;
+    }
+    
+  }
+
+  // apply powerups
+  if (powerUp) {
+    let num = wallNumbers[0] + 1;
+    if (num > 3) num -= 4;
+    wallNumbers.push(num);
+  }
+
+  if (parallelUp) {
+    let num = wallNumbers[0] + 2;
+    if (num > 3) num -= 4;
+    wallNumbers.push(num);
+  }
+
 }
